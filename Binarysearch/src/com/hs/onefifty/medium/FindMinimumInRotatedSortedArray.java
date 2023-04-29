03c3a51b2b1532c5deaf11003155396bd0b3ad33
Hareram Singh <hareramcse@gmail.com>
2023-04-29 09:29:10 +0530

    Latest

---
+++ Binarysearch/src/com/hs/onefifty/medium/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,21 @@
+package com.hs.onefifty.medium;
+
+public class FindMinimumInRotatedSortedArray {
+	public int findMin(int[] nums) {
+		int start = 0;
+		int end = nums.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			int prev = (mid - 1 + nums.length) % nums.length;
+			int next = (mid + 1) % nums.length;
+			if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
+				return nums[mid];
+			} else if (nums[mid] <= nums[end]) {
+				end = mid - 1;
+			} else if (nums[mid] >= nums[start]) {
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
+}
---
+++ Binarysearch/src/com/hs/onefifty/medium/SearchA2DMatrix.java
@@ -0,0 +1,24 @@
+package com.hs.onefifty.medium;
+
+public class SearchA2DMatrix {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		int i = 0, j = matrix[0].length - 1;
+		while (i < matrix.length && j >= 0) {
+			if (target == matrix[i][j])
+				return true;
+			else if (target < matrix[i][j])
+				j--;
+			else
+				i++;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		SearchA2DMatrix obj = new SearchA2DMatrix();
+		int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 16;
+		boolean result = obj.searchMatrix(matrix, target);
+		System.out.println(result);
+	}
+}
